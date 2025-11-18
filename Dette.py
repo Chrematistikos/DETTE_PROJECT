38e815c8df0f54bdb05c0d0204bec343d0a8c1fb
Chrematistikos <ghanicherrak@outlook.fr>
2025-11-18 23:11:51 +0100

    Version 2 (menu)

--- Dette.py
+++ Dette.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 st.title("Simulation de la dette publique")
 
 # Paramètres ajustables
-r = st.sidebar.number_input("Taux d'intérêt actuel (r)", value=0.025, step=0.001)
-g = st.sidebar.number_input("Taux de croissance actuel (g)", value=0.018, step=0.001)
-s0 = st.sidebar.number_input("Solde primaire actuel (% PIB)", value=-0.032, step=0.01)
-x0 = st.sidebar.number_input("Dette actuelle (% PIB)", value=1.15, step=0.01)
+r = st.sidebar.number_input("Taux d'intérêt actuel (r)", value=2.25, step=0.01)/100
+g = st.sidebar.number_input("Taux de croissance actuel (g)", value=1.8, step=0.1)/100
+s0 = st.sidebar.number_input("Solde primaire actuel (% PIB)", value=-3.2, step=0.1)/100
+x0 = st.sidebar.number_input("Dette actuelle (% PIB)", value=115, step=0.5)/100
 a0 = st.sidebar.number_input("Année actuelle", value=2025, step=1)
-x_Obj = st.sidebar.number_input("Objectif de dette (% PIB)", value=1.0, step=0.01)
+x_Obj = st.sidebar.number_input("Objectif de dette (% PIB)", value=100, step=0.5)/100
 t = st.sidebar.number_input("Durée de projection pour la trajectoire  (années)", value=5, step=1)
 n = st.sidebar.number_input("Durée pour atteindre l'objectif de dette (années)", value=10, step=1)
-effort = st.sidebar.number_input("Effort annuel pour l'ajustement progressif", value=0.005, step=0.001)
+effort = st.sidebar.number_input("Effort annuel pour l'ajustement progressif (% PIB)", value=0.005, step=0.001)/100
 
 
 # Menu
